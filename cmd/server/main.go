5ffe24f0743212cd05643bd3790b5c20ff891290
speady1445 <sykora1445@gmail.com>
2024-08-30 22:03:33 +0200

    Add interactive options to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
+	"github.com/speady1445/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/speady1445/learn-pub-sub-starter/internal/pubsub"
 	"github.com/speady1445/learn-pub-sub-starter/internal/routing"
 
@@ -18,33 +16,62 @@ func main() {
 	connection_string := "amqp://guest:guest@localhost:5672/"
 	connection, err := amqp.Dial(connection_string)
 	if err != nil {
-		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer connection.Close()
+	fmt.Println("Successfully connected to RabbitMQ!")
 
 	channel, err := connection.Channel()
 	if err != nil {
-		log.Fatalf("Could not open channel: %v", err)
+		log.Fatalf("could not open channel: %v", err)
 	}
 
-	err = pubsub.PublishJSON(
+	gamelogic.PrintServerHelp()
+
+	for {
+		words := gamelogic.GetInput()
+
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			fmt.Println("Pausing...")
+			pause(channel)
+		case "resume":
+			fmt.Println("Resuming...")
+			unpause(channel)
+		case "quit":
+			return
+		case "help":
+			gamelogic.PrintServerHelp()
+		default:
+			fmt.Println("Me not speak you tongue!?")
+		}
+	}
+}
+
+func pause(channel *amqp.Channel) {
+	err := pubsub.PublishJSON(
 		channel,
 		routing.ExchangePerilDirect,
 		routing.PauseKey,
 		routing.PlayingState{IsPaused: true},
 	)
 	if err != nil {
-		log.Fatalf("Could not publish message: %v", err)
+		log.Printf("could not publish message: %v", err)
 	}
-
-	fmt.Println("Successfully connected to RabbitMQ!")
-	wait_for_user_interupt()
 }
 
-func wait_for_user_interupt() {
-	fmt.Println("Press CTRL+C to exit...")
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
-	<-done // Will block here until user hits ctrl+c
-	fmt.Println("Exiting now...")
+func unpause(channel *amqp.Channel) {
+	err := pubsub.PublishJSON(
+		channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: false},
+	)
+	if err != nil {
+		log.Printf("could not publish message: %v", err)
+	}
 }
