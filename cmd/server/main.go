4fb047bb3bc90c3ecaf470318295294c0e213007
speady1445 <sykora1445@gmail.com>
2024-08-30 11:46:08 +0200

    Add server connection to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connection_string := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connection_string)
+	if err != nil {
+		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Successfully connected to RabbitMQ!")
+	wait_for_user_interupt()
+}
+
+func wait_for_user_interupt() {
+	fmt.Println("Press CTRL+C to exit...")
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done // Will block here until user hits ctrl+c
+	fmt.Println("Exiting now...")
 }
